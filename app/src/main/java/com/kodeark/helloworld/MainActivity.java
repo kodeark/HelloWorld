ea90b96b080d48f68ee1ea75397651c9c4111304
David <david@moacreative.com>
2016-03-29 15:58:22 +0530

    Added a comment to the main activity

--- app/src/main/java/com/kodeark/helloworld/MainActivity.java
+++ app/src/main/java/com/kodeark/helloworld/MainActivity.java
@@ -1,13 +1,15 @@
 package com.kodeark.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This is layout file i m linking
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/kodeark/helloworld/Test.java
@@ -0,0 +1,9 @@
+package com.kodeark.helloworld;
+
+/**
+ * Created by David on 29/03/16.
+ */
+public class Test {
+
+    public String name;
+}
